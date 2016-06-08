898cfbb1f1631087c733bdcf0cd16fcf7a2b71c0
chengxincheung <1193055507@qq.com>
2016-06-08 14:28:06 +0800

    Add files via upload

---
+++ js-rem.js
@@ -0,0 +1,18 @@
+;(function(window){
+    var doc = window.document, 
+        docEle = doc.documentElement,
+        designWidth = 960,                  //设计图宽度
+        baseWidth = 100,                    //基准宽度
+        maxWidth = 640,                     //最大宽度
+        resize = 'orientationchange' in window ? 'orientationchange' : 'resize', 
+        resizeEvent = function(){
+            var pageWidth = docEle.clientWidth;
+            if( pageWidth > maxWidth ){
+                pageWidth = maxWidth;
+            }
+            docEle.style.fontSize = baseWidth * ( pageWidth / designWidth ) + 'px';
+        };
+
+    doc.addEventListener('DOMContentLoaded', resizeEvent, false);
+    window.addEventListener( resize, resizeEvent, false); 
+})(window);
