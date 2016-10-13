419a1f0d1085384c91635e98fa69538bd14324f0
Mark Chang <1193055507@qq.com>
2016-10-13 18:17:47 +0800

    Update js-rem.js

--- js-rem.js
+++ js-rem.js
@@ -1,11 +1,10 @@
 ;(function(window){
-    var doc = window.document, 
-        docEle = doc.documentElement,
+    var document = window.document, 
+        docEle = document.documentElement,
         designWidth = 960,                  //设计图宽度
         baseWidth = 100,                    //基准宽度
         maxWidth = 640,                     //最大宽度
-        resize = 'onorientationchange' in window ? 'orientationchange' : 'resize', 
-        resizeHandler = function(){
+        callback = function(){
             var pageWidth = docEle.clientWidth;
             if( maxWidth && (pageWidth > maxWidth) ){
                 pageWidth = maxWidth;
@@ -13,6 +12,6 @@
             docEle.style.fontSize = baseWidth * ( pageWidth / designWidth ) + 'px';
         };
 
-    doc.addEventListener('DOMContentLoaded', resizeHandler, false);
-    window.addEventListener( resize, resizeHandler, false); 
+    window.addEventListener('DOMContentLoaded', callback);
+    window.addEventListener( 'resize', callback); 
 })(window);
